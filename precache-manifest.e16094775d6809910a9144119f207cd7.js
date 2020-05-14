288e375425e8171a0cc551cc7553fec877089e4c
jedrazb <jedrazb@gmail.com>
2020-05-14 15:25:07 +0100

    Updates

---
+++ precache-manifest.e16094775d6809910a9144119f207cd7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75070c910934ce831c2d5de0f883373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f6c9da7589893503412",
+    "url": "/static/css/main.397a04a0.chunk.css"
+  },
+  {
+    "revision": "373cbdf3f73a4f51ba38",
+    "url": "/static/js/2.cc8a788b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cc8a788b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f6c9da7589893503412",
+    "url": "/static/js/main.01ee09ea.chunk.js"
+  },
+  {
+    "revision": "e914317c63998b4ea853",
+    "url": "/static/js/runtime-main.da38689f.js"
+  },
+  {
+    "revision": "8796b7875d52e9cb9b280211e8d3f82d",
+    "url": "/static/media/merriweather-400-l.8796b787.woff2"
+  },
+  {
+    "revision": "5751237264c0468be0d48ad1cea75b90",
+    "url": "/static/media/montserrat-900-l.57512372.woff2"
+  },
+  {
+    "revision": "c243d312fc407e62e7a7b7d5101bd5bd",
+    "url": "/static/media/profile-pic.c243d312.jpg"
+  }
+]);
