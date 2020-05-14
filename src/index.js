767981c8d2dc67ad36ece5a56dd41fe697057f88
jedrazb <jedrazb@gmail.com>
2020-05-14 17:21:05 +0100

    adding GA

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("UA-61071111-3");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
